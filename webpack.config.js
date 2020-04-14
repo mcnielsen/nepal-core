2dfb27096a047966d93c3191c06ff2abe7e697fe
Jason Tarver <jtarver@alertlogic.com>
2020-04-14 15:27:30 -0500

    sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -5,24 +5,35 @@ module.exports = (env) => {
 
     return {
         mode     : 'production',
+        devtool: 'source-map',
         entry    : {
-            'index': path.resolve(__dirname, './dist/commonjs/index.js'),
+            'index': path.resolve(__dirname, './index.ts'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
         },
         resolve  : {
+            extensions: [ '.tsx', '.ts', '.js' ],
             alias: {
-                "@al/client"       : path.resolve(__dirname,"./dist/commonjs/src/api-client"),
-                "@al/aims"         : path.resolve(__dirname,"./dist/commonjs/src/aims-client"),
-                "@al/common"       : path.resolve(__dirname,"./dist/commonjs/src/nepal-common"),
-                "@al/search"       : path.resolve(__dirname,"./dist/commonjs/src/search-client"),
-                "@al/session"      : path.resolve(__dirname,"./dist/commonjs/src/session-client"),
-                "@al/subscriptions": path.resolve(__dirname,"./dist/commonjs/src/subscriptions-client"),
+                "@al/client"       : path.resolve(__dirname,"./src/api-client"),
+                "@al/aims"         : path.resolve(__dirname,"./src/aims-client"),
+                "@al/common"       : path.resolve(__dirname,"./src/nepal-common"),
+                "@al/search"       : path.resolve(__dirname,"./src/search-client"),
+                "@al/session"      : path.resolve(__dirname,"./src/session-client"),
+                "@al/subscriptions": path.resolve(__dirname,"./src/subscriptions-client"),
             },
         },
         externals: [nodeExternals()],
         output   : {
             path         : path.resolve(__dirname, './dist/umd'),
             filename     : '[name].js',
-            library      : 'aimsClient',
+            library      : '@al/core',
             libraryTarget: 'umd', // supports commonjs, amd and web browsers
             globalObject : 'this',
         },
