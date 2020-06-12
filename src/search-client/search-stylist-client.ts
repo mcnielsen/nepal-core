0f07b1fc91c80e8fb40d456709d1704dc8cb2468
Jorge Mario Valencia <jmvalencia@users.noreply.github.com>
2020-06-12 14:53:33 -0500

    ENG-5593: Changing the call to use json endpoint and adding the csv logic to handle export (#31)
    * ENG-5593: Changing the call to use json endpoint and adding the csv logic to handle export
    
    * Bumping version

--- src/search-client/search-stylist-client.ts
+++ src/search-client/search-stylist-client.ts
@@ -18,14 +18,25 @@ class AlSearchStylist {
      *  search_stylist will request results from EM search service by calling Search Results
      *  endpoint for the same account_id/uuid with the same query string params (offset/limit/etc) as passed to this endpoint.
      */
-    searchStylist(accountId: string, uuid: string, additionalParams?: AlSearchResultsQueryParamsV2): Promise<AlSearchGetV2> {
+    searchStylist(accountId: string, uuid: string, type: 'json'|'csv' = 'json', additionalParams?: AlSearchResultsQueryParamsV2): Promise<AlSearchGetV2|any> {
         const fetchRequestArgs: APIRequestParams = {
             service_name: this.serviceName,
             account_id: accountId,
-            path: `/searches/${uuid}`
+            path: `/searches/${uuid}/transform/${type}`
         };
         if (additionalParams) {
             fetchRequestArgs.params = additionalParams;
+        }
+        // In case we are trying to fetch csv we will return the blob file this
+        // will help the consumer to handle the response and export easily
+        if (type === 'csv') {
+            if ( !fetchRequestArgs.headers ) {
+                fetchRequestArgs.headers = {};
+            }
+            fetchRequestArgs.headers.Accept = 'text/csv';
+            fetchRequestArgs.responseType = 'blob';
+
+            return ALClient.get(fetchRequestArgs);
         }
         return ALClient.get<AlSearchGetV2>(fetchRequestArgs);
     }
