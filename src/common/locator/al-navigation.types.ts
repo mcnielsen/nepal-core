841cad9fdcff1c32328d0763b4f226605d98bc2c
mcnielsen <knielsen@alertlogic.com>
2020-07-08 10:59:22 -0700

    ENG-7117 - Experience Flag Type Support (#45)
    Expands `AlExperienceMapping` to include an optional set of "triggers"
    that can be used to determine whether an experience should be available
    under certain fixed conditions -- by environment, account, and time.

--- src/common/locator/al-navigation.types.ts
+++ src/common/locator/al-navigation.types.ts
@@ -21,15 +21,42 @@ export interface AlNavigationSchema
     conditions: {[conditionId:string]:AlRouteCondition};
 }
 
+export interface AlExperienceToggle
+{
+    after?:string|number;
+    before?:string|number;
+    accounts?:string[];
+    environments?:string[];
+}
+
 /**
  * Describes the entry location, availability criteria, and prompt behaviors of a specific experience (for the purposes of this
  * type, and "experience" = a specific variation or common subset of a feature.
  */
 export interface AlExperienceMapping
 {
-    name:string;                    //  Decorative
-    entryRoute:AlRouteAction;       //  A trigger or link that invokes the experience
-    availabilityQuery:string|any;   //  A searchlib compatible expression determining whether the experience is available to a given context, in either raw or SQL-style form
+    /*
+     * A descriptive name for the experience
+     */
+    name:string;
+
+    /**
+     * A route or trigger that can be used to imperatively invoke the experience
+     */
+    entryRoute:AlRouteAction;
+
+    /**
+     * A searchlib compatible expression determining whether the experience is available to a given context,
+     * in either raw or SQL-style form.
+     */
+    availabilityQuery:string|any;
+
+    /**
+     * An optional boolean, toggle, or array of toggles that determine when this experience should be enabled.  If provided,
+     * a positive match will supercede `availabilityQuery`.  This can be used to schedule enablement for specific customers
+     * in specific environments in advance of the addition of an entitlement.
+     */
+    trigger:AlExperienceToggle|AlExperienceToggle[]|boolean;
 
     /**
      * If provided, defines the zero state to provide when an experience isn't available.
