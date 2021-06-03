07ba31022dd32b9e3268f644232d64816d7c11a4
Maryit Viviana Sanchez Vivas <msanchez@alertlogic.com>
2021-06-03 13:40:49 -0500

    US000: adding dropdown to responder options (#158)

--- src/common/forms/types.ts
+++ src/common/forms/types.ts
@@ -5,7 +5,7 @@ export interface AlDynamicFormControlElementOptions {
 }
 
 export interface AlDynamicFormControlInputResponderOptions {
-    type?: 'input' | 'textarea';
+    type?: 'input' | 'textarea' | 'dropdown';
     buttonLabel?: string;
     buttonTooltip?: string;
     options?: {
